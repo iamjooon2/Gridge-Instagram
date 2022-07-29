c6f017c257436a5b48285ecc5c38dcd54303897b
iamjooon2 <iamjooon2@gmail.com>
2022-07-30 02:25:57 +0900

    build: db connection pool options 추가

--- src/assets/db.js
+++ src/assets/db.js
@@ -18,6 +18,43 @@ const pool = mysql.createPool({
     bigNumberStrings: true,
     multipleStatements: true,
 
+    multipleStatements: true,
+
+    // ----- connection pool options
+  
+    /**
+     * The milliseconds before a timeout occurs during the initial connection to the MySQL server. (Default: 10 seconds)
+     */
+     connectTimeout: 10 * 1000,
+  
+    /**
+    * Determines the pool's action when no connections are available and the limit has been reached. If true, the pool will queue
+    * the connection request and call it when one becomes available. If false, the pool will immediately call back with an error.
+    * (Default: true)
+    */
+    waitForConnections: true,
+  
+    /**
+    * The maximum number of connections to create at once. (Default: 10)
+    */
+    connectionLimit: 5,
+  
+    /**
+    * The maximum number of connection requests the pool will queue before returning an error from getConnection. If set to 0, there
+    * is no limit to the number of queued connection requests. (Default: 0)
+    */
+    queueLimit: 1,
+  
+    /**
+    * Enable keep-alive on the socket.  It's disabled by default, but the
+    * user can enable it and supply an initial delay.
+    */
+    enableKeepAlive: true,
+  
+    /**
+    * If keep-alive is enabled users can supply an initial delay.
+    */
+    keepAliveInitialDelay: 5,
 });
 
 
