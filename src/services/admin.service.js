a58a823eadf9578f6f37d193b7dae6c1b1d82508
iamjooon2 <iamjooon2@gmail.com>
2022-08-12 11:03:44 +0900

    fix: mistypo

--- src/services/admin.service.js
+++ src/services/admin.service.js
@@ -23,7 +23,7 @@ exports.retrieveUserList = async (id, name, signUpDate, status, page) => {
             whereQuery += ` AND ID = ${id}`;
         }
         if (status !== undefined) {
-            whereQuery = ` AND status = ${status}`
+            whereQuery += ` AND status = ${status}`
         }
         if (signUpDate !== undefined) {
             let date = signUpDate.replace(/(\d{4})(\d{2})(\d{2})/,`'$1-$2-$3'`)
