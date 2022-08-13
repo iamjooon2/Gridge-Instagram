3622a6b4dee82f466a1e1afa082ced40e0e49276
iamjooon2 <iamjooon2@gmail.com>
2022-08-13 18:22:29 +0900

    refactor: util 패키지에 정규표현식 빼기

--- src/controllers/admin.controller.js
+++ src/controllers/admin.controller.js
@@ -7,7 +7,7 @@ const regexDate = new RegExp(/(^(19|20)\d{2})(0[1-9]|1[0-2])(0[1-9]|[12][0-9]|3[
 // 회원 전체 정보 검색
 class AdminController {
     constructor (){
-        this.AdminController
+        this.AdminController;
     }
 }
 
