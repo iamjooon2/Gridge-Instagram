b5f812fa5d96eeb84acc796be8db0ef9cf208fe8
iamjooon2 <iamjooon2@gmail.com>
2022-08-24 02:03:49 +0900

    refactor: Controller Layer classification

--- src/controllers/message.controller.js
+++ src/controllers/message.controller.js
@@ -1,53 +1,60 @@
 const MessageService = require('../services/message.service');
+
 const baseResponse = require('../utilities/baseResponseStatus');
 const {errResponse, response} = require('../utilities/response');
 
-const postMessage = async (req, res) => {
-    const toeknIdxData = req.verifiedToken.idx;
-    const userIdx = toeknIdxData[0].userIdx;
-    const partnerIdx = req.body.partnerIdx;
-    const content = req.body.content;
+class MessageController {
+    MessageService;
 
-    // validation
-    if (!partnerIdx) {
-        return res.send(errResponse(baseResponse.MESSAGE_USERIDX_EMPTY));
-    } else if (partnerIdx < 0){
-        return res.send(errResponse(baseResponse.MESSAGE_USERIDX_LENGTH));
+    constructor(){
+        this.MessageService = new MessageService();
     }
 
-    // validation
-    if (!content) {
-        return res.send(errResponse(baseResponse.MESSAGE_CONTENT_EMPTY));
-    } else if (content.length >200) {
-        return res.send(errResponse(baseResponse.MESSAGE_CONTENT_LENGTH));
+    postMessage = async (req, res) => {
+        const toeknIdxData = req.verifiedToken.idx;
+        const userIdx = toeknIdxData[0].userIdx;
+        const partnerIdx = req.body.partnerIdx;
+        const content = req.body.content;
+
+        // validation
+        if (!partnerIdx) {
+            return res.send(errResponse(baseResponse.MESSAGE_USERIDX_EMPTY));
+        } else if (partnerIdx < 0){
+            return res.send(errResponse(baseResponse.MESSAGE_USERIDX_LENGTH));
+        }
+
+        // validation
+        if (!content) {
+            return res.send(errResponse(baseResponse.MESSAGE_CONTENT_EMPTY));
+        } else if (content.length >200) {
+            return res.send(errResponse(baseResponse.MESSAGE_CONTENT_LENGTH));
+        }
+
+        const postMessageResult = await this.MessageService.postMessage(userIdx, partnerIdx, content);
+
+        return res.send(postMessageResult);
+
     }
 
-    const postMessageResult = await MessageService.postMessage(userIdx, partnerIdx, content);
+    getMessages = async (req, res) => {
 
-    return res.send(postMessageResult);
+        const toeknIdxData = req.verifiedToken.idx;
+        const userIdx = toeknIdxData[0].userIdx;
+        const partnerIdx = req.body.partnerIdx;
+
+        // validation
+        if (!partnerIdx) {
+            return res.send(errResponse(baseResponse.MESSAGE_USERIDX_EMPTY));
+        } else if (partnerIdx < 0){
+            return res.send(errResponse(baseResponse.MESSAGE_USERIDX_LENGTH));
+        }
+
+        const totalMessageResult = await this.MessageService.getMessages(userIdx, partnerIdx);
+
+        return res.send(totalMessageResult);
+    }
 
 }
 
-const getMessages = async (req, res) => {
 
-    const toeknIdxData = req.verifiedToken.idx;
-    const userIdx = toeknIdxData[0].userIdx;
-    const partnerIdx = req.body.partnerIdx;
-
-    // validation
-    if (!partnerIdx) {
-        return res.send(errResponse(baseResponse.MESSAGE_USERIDX_EMPTY));
-    } else if (partnerIdx < 0){
-        return res.send(errResponse(baseResponse.MESSAGE_USERIDX_LENGTH));
-    }
-
-    const totalMessageResult = await messageService.getMessages(userIdx, partnerIdx);
-
-    return res.send(totalMessageResult);
-}
-
-
-module.exports = {
-    postMessage,
-    getMessages
-};
+module.exports = MessageController;
--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -1,223 +1,221 @@
-const postService = require('../services/post.service');
+const PostService = require('../services/post.service');
 
 const baseResponse = require('../utilities/baseResponseStatus');
 const {errResponse, response} = require('../utilities/response');
 
 
-// 게시글 등록
-const postPost = async (req, res) => {
+class PostController {
+    PostService;
 
-    const userIdxFromToken = req.verifiedToken.idx;
-
-    const { userIdx, postImgUrls, content} = req.body;
-
-    // Authentication
-    if ( userIdxFromToken[0].userIdx != userIdx){
-        return res.send(errResponse(baseResponse.USER_ID_NOT_MATCH));
+    constructor(){
+        this.PostService = new PostService();
     }
 
-    // validation
-    if (!userIdx) {
-        return res.send(errResponse(baseResponse.USER_USERIDX_EMPTY));
-    } else  if (userIdx <= 0) {
-        return res.send(errResponse(baseResponse.USER_USERIDX_LENGTH));
-    } 
+        // 게시글 등록
+    postPost = async (req, res) => {
 
-    if (postImgUrls.length <= 0) {
-        return res.send(errResponse(baseResponse.POST_POSTIMGURLS_EMPTY));
+        const userIdxFromToken = req.verifiedToken.idx;
+
+        const { userIdx, postImgUrls, content} = req.body;
+
+        // Authentication
+        if ( userIdxFromToken[0].userIdx != userIdx){
+            return res.send(errResponse(baseResponse.USER_ID_NOT_MATCH));
+        }
+
+        // validation
+        if (!userIdx) {
+            return res.send(errResponse(baseResponse.USER_USERIDX_EMPTY));
+        } else  if (userIdx <= 0) {
+            return res.send(errResponse(baseResponse.USER_USERIDX_LENGTH));
+        } 
+
+        if (postImgUrls.length <= 0) {
+            return res.send(errResponse(baseResponse.POST_POSTIMGURLS_EMPTY));
+        }
+
+        if (!content){
+            return res.send(errResponse(baseResponse.POST_CONTENT_EMPTY));
+        } else if (content.length > 1000) {
+            return res.send(errResponse(baseResponse.POST_CONTENT_LENGTH));
+        }
+
+        const createdPostResult = await postService.createPost(
+            userIdx,
+            postImgUrls,
+            content
+        );
+
+        return res.send(response(baseResponse.SUCCESS));
     }
 
-    if (!content){
-        return res.send(errResponse(baseResponse.POST_CONTENT_EMPTY));
-    } else if (content.length > 1000) {
-        return res.send(errResponse(baseResponse.POST_CONTENT_LENGTH));
+    // 게시글 수정
+    patchPost = async (req, res) => {
+
+        const idx = req.verifiedToken.idx;
+        const postIdx = req.params.postIdx;
+        const content = req.body.content;
+
+        const writerOfPost = await postService.retrieveUserIdx(postIdx);
+
+        if (writerOfPost[0].userIdx !== idx[0].userIdx) {
+            return res.send(errResponse(baseResponse.USER_ID_NOT_MATCH));
+        }
+
+        if(!postIdx) {
+            return res.send(errResponse(baseResponse.POST_POSTIDX_EMPTY));
+        }else if (postIdx < 1) {
+            return res.send(errResponse(baseResponse.POST_POSTIDX_LENGTH));
+        }
+
+        if(!content) {
+            return res.send(errResponse(baseResponse.POST_CONTENT_EMPTY))
+        }else if (content.length > 1000) {
+            return res.send(errResponse(baseResponse.POST_CONTENT_LENGTH));
+        } 
+
+        const editPostResponse = await postService.updatePost(content, postIdx);
+
+        return res.send(editPostResponse);
     }
 
-    const createdPostResult = await postService.createPost(
-        userIdx,
-        postImgUrls,
-        content
-    );
+    // 게시글 조회
+    getPosts = async (req, res) => {
+        
+        const userIdx = req.params.userIdx;
+        const page = req.query.page;
 
-    return res.send(response(baseResponse.SUCCESS));
+        // validation
+        if(!userIdx) {
+            return res.send(errResponse(baseResponse.USER_USERIDX_EMPTY));
+        } else if (userIdx <= 0) {
+            return res.send(errResponse(baseResponse.USER_USERIDX_LENGTH));
+        }
+
+        if (!page) {
+            return res.send(errResponse(baseResponse.PAGENATION_ERROR));
+        } else  if (page <= 0) {
+            return res.send(errResponse(baseResponse.PAGENATION_ERROR));
+        }
+
+        const postListResult = await postService.retrievePostLists(userIdx, page);
+
+        return res.send(response(baseResponse.SUCCESS, postListResult));
+    }
+
+    // 게시글 내용 조회
+    getPostContent = async (req, res) => {
+
+        const postIdx = req.params.postIdx;
+
+        // validation
+        if (!postIdx){
+            return res.send(errResponse(baseResponse.POST_POSTIDX_EMPTY));
+        } else if(postIdx < 1) {
+            return res.send(errResponse(baseResponse.POST_POSTIDX_LENGTH));
+        }
+
+        const postContent = await postService.retrievePostContent(postIdx);
+
+        return res.send(postContent);
+
+    }
+
+    // 게시글 삭제
+    patchPostStatus = async (req ,res) => {
+
+        const idx = req.verifiedToken.idx;
+        const postIdx = req.params.postIdx;
+
+        const writerOfPost = await postService.retrieveUserIdx(postIdx);
+
+        // Authentication
+        if (writerOfPost[0].userIdx !== idx[0].userIdx) {
+            return res.send(errResponse(baseResponse.WRITER_NOT_MATCH));
+        }
+        
+        // validation
+        if (!postIdx) {
+            return res.send(errResponse(baseResponse.POST_POSTIDX_EMPTY));
+        } else if (postIdx <= 0) {
+            return res.send(errResponse(baseResponse.POST_POSTIDX_LENGTH));
+        }
+
+        const editPostStatusResponse = await postService.updatePostStatus(postIdx);
+
+        return res.send(editPostStatusResponse);
+    }
+
+    // 게시글 좋아요
+    postPostLike = async (req, res) => {
+
+        const idx = req.verifiedToken.idx;
+        const postIdx = req.params.postIdx;
+
+        const userIdx = idx[0].userIdx;
+
+        if(!postIdx) {
+            return res.send(errResponse(baseResponse.POST_POSTIDX_EMPTY));
+        } else if (postIdx < 1) {
+            return res.send(errResponse(baseResponse.POST_POSTIDX_LENGTH));
+        }
+
+        const postLikeResponse = await postService.createPostLike(userIdx, postIdx);
+
+        return res.send(postLikeResponse);
+    }
+
+    // 게시글 좋아요 해제
+    postPostDislike = async (req, res) => {
+        
+        const idx = req.verifiedToken.idx;
+        const postIdx = req.params.postIdx;
+
+        const userIdx = idx[0].userIdx;
+
+        if(!postIdx) {
+            return res.send(errResponse(baseResponse.POST_POSTIDX_EMPTY));
+        } else if (postIdx < 1) {
+            return res.send(errResponse(baseResponse.POST_POSTIDX_LENGTH));
+        }
+
+        const postDislikeResponse = await postService.createPostDislike(userIdx, postIdx);
+
+        return res.send(postDislikeResponse);
+    }  
+
+    // 게시글 신고
+    postPostReport = async (req, res) => {
+
+        const idx = req.verifiedToken.idx;
+        const postIdx = req.params.postIdx;
+        // 신고구분: 1-욕설, 2-스팸 등의 숫자로 구분한 코드를 클라이언트로부터 넘겨받는다 가정
+        const reportCode = req.query.reportCode;
+
+        const userIdx = idx[0].userIdx;
+
+        // postIdx validation
+        if (!postIdx) {
+            return res.send(errResponse(baseResponse.POST_POSTIDX_EMPTY));
+        } else if (postIdx < 1) {
+            return res.send(errResponse(baseResponse.POST_POSTIDX_LENGTH));
+        }
+
+        // 신고구분 validation
+        if(!reportCode) {
+            return res.send(errResponse(baseResponse.REPORT_CODE_EMPTY));
+        }
+
+        //자신의 게시글을 신고하려는지 확인
+        const checkMyPostResult = await postService.retrieveUserIdx(postIdx);
+        if (userIdx==checkMyPostResult) {
+            return res.send(errResponse(baseResponse.REPORT_MYPOST));
+        }
+
+        const postReportResponse = await postService.createPostReport(userIdx, postIdx, reportCode);
+
+        return res.send(postReportResponse);
+}
 }
 
-// 게시글 수정
-const patchPost = async (req, res) => {
-
-    const idx = req.verifiedToken.idx;
-    const postIdx = req.params.postIdx;
-    const content = req.body.content;
-
-    const writerOfPost = await postService.retrieveUserIdx(postIdx);
-
-    if (writerOfPost[0].userIdx !== idx[0].userIdx) {
-        return res.send(errResponse(baseResponse.USER_ID_NOT_MATCH));
-    }
-
-    if(!postIdx) {
-        return res.send(errResponse(baseResponse.POST_POSTIDX_EMPTY));
-    }else if (postIdx < 1) {
-        return res.send(errResponse(baseResponse.POST_POSTIDX_LENGTH));
-    }
-
-    if(!content) {
-        return res.send(errResponse(baseResponse.POST_CONTENT_EMPTY))
-    }else if (content.length > 1000) {
-        return res.send(errResponse(baseResponse.POST_CONTENT_LENGTH));
-    } 
-
-    const editPostResponse = await postService.updatePost(content, postIdx);
-
-    return res.send(editPostResponse);
-}
-
-// 게시글 조회
-const getPosts = async (req, res) => {
-    
-    const userIdx = req.params.userIdx;
-    const page = req.query.page;
-
-    // validation
-    if(!userIdx) {
-        return res.send(errResponse(baseResponse.USER_USERIDX_EMPTY));
-    } else if (userIdx <= 0) {
-        return res.send(errResponse(baseResponse.USER_USERIDX_LENGTH));
-    }
-
-    if (!page) {
-        return res.send(errResponse(baseResponse.PAGENATION_ERROR));
-    } else  if (page <= 0) {
-        return res.send(errResponse(baseResponse.PAGENATION_ERROR));
-    }
-
-    const postListResult = await postService.retrievePostLists(userIdx, page);
-
-    return res.send(response(baseResponse.SUCCESS, postListResult));
-}
-
-// 게시글 내용 조회
-const getPostContent = async (req, res) => {
-
-    const postIdx = req.params.postIdx;
-
-    // validation
-    if (!postIdx){
-        return res.send(errResponse(baseResponse.POST_POSTIDX_EMPTY));
-    } else if(postIdx < 1) {
-        return res.send(errResponse(baseResponse.POST_POSTIDX_LENGTH));
-    }
-
-    const postContent = await postService.retrievePostContent(postIdx);
-
-    return res.send(postContent);
-
-}
-
-// 게시글 삭제
-const patchPostStatus = async (req ,res) => {
-
-    const idx = req.verifiedToken.idx;
-    const postIdx = req.params.postIdx;
-
-    const writerOfPost = await postService.retrieveUserIdx(postIdx);
-
-    // Authentication
-    if (writerOfPost[0].userIdx !== idx[0].userIdx) {
-        return res.send(errResponse(baseResponse.WRITER_NOT_MATCH));
-    }
-    
-    // validation
-    if (!postIdx) {
-        return res.send(errResponse(baseResponse.POST_POSTIDX_EMPTY));
-    } else if (postIdx <= 0) {
-        return res.send(errResponse(baseResponse.POST_POSTIDX_LENGTH));
-    }
-
-    const editPostStatusResponse = await postService.updatePostStatus(postIdx);
-
-    return res.send(editPostStatusResponse);
-}
-
-// 게시글 좋아요
-const postPostLike = async (req, res) => {
-
-    const idx = req.verifiedToken.idx;
-    const postIdx = req.params.postIdx;
-
-    const userIdx = idx[0].userIdx;
-
-    if(!postIdx) {
-        return res.send(errResponse(baseResponse.POST_POSTIDX_EMPTY));
-    } else if (postIdx < 1) {
-        return res.send(errResponse(baseResponse.POST_POSTIDX_LENGTH));
-    }
-
-    const postLikeResponse = await postService.createPostLike(userIdx, postIdx);
-
-    return res.send(postLikeResponse);
-}
-
-// 게시글 좋아요 해제
-const postPostDislike = async (req, res) => {
-    
-    const idx = req.verifiedToken.idx;
-    const postIdx = req.params.postIdx;
-
-    const userIdx = idx[0].userIdx;
-
-    if(!postIdx) {
-        return res.send(errResponse(baseResponse.POST_POSTIDX_EMPTY));
-    } else if (postIdx < 1) {
-        return res.send(errResponse(baseResponse.POST_POSTIDX_LENGTH));
-    }
-
-    const postDislikeResponse = await postService.createPostDislike(userIdx, postIdx);
-
-    return res.send(postDislikeResponse);
-}  
-
-// 게시글 신고
-const postPostReport = async (req, res) => {
-
-    const idx = req.verifiedToken.idx;
-    const postIdx = req.params.postIdx;
-    // 신고구분: 1-욕설, 2-스팸 등의 숫자로 구분한 코드를 클라이언트로부터 넘겨받는다 가정
-    const reportCode = req.query.reportCode;
-
-    const userIdx = idx[0].userIdx;
-    console.log(userIdx);
-
-    // postIdx validation
-    if (!postIdx) {
-        return res.send(errResponse(baseResponse.POST_POSTIDX_EMPTY));
-    } else if (postIdx < 1) {
-        return res.send(errResponse(baseResponse.POST_POSTIDX_LENGTH));
-    }
-
-    // 신고구분 validation
-    if(!reportCode) {
-        return res.send(errResponse(baseResponse.REPORT_CODE_EMPTY));
-    }
-
-    //자신의 게시글을 신고하려는지 확인
-    const checkMyPostResult = await postService.retrieveUserIdx(postIdx);
-    if (userIdx==checkMyPostResult) {
-        return res.send(errResponse(baseResponse.REPORT_MYPOST));
-    }
-
-    const postReportResponse = await postService.createPostReport(userIdx, postIdx, reportCode);
-
-    return res.send(postReportResponse);
-}
-
-module.exports = {
-    postPost,
-    patchPost,
-    getPosts,
-    getPostContent,
-    patchPostStatus,
-    postPostLike,
-    postPostDislike,
-    postPostReport
-};
+module.exports = PostController;
