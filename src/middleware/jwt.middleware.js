98569031d92a54dc45ede00d79a31dd99ed9312f
iamjooon2 <iamjooon2@gmail.com>
2022-09-27 21:56:44 +0900

    fix: 미들웨어내 userService 클래스로 변경

--- src/middleware/jwt.middleware.js
+++ src/middleware/jwt.middleware.js
@@ -1,11 +1,12 @@
 const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
 dotenv.config();
-const { response, errResponse } = require('../utility/response');
-const userService = require('../service/user.service');
+const UserService = require('../service/user.service');
+const errResponse = require('../utility/response');
 const baseResponse = require('../utility/baseResponseStatus');
 
 const jwtMiddleware = (req, res, next) => {
+    this.UserService = new UserService();
     
     let accessToken = req.headers['x-access-token'] || req.headers['authorization'];
     if (!accessToken){
@@ -20,9 +21,9 @@ const jwtMiddleware = (req, res, next) => {
     //토큰 검증
     const p = new Promise(
         (resolve, reject) => {
-            jwt.verify(token, process.env.JWT_SECRET , (error, verifiedToken) => {
+            jwt.verify(token, process.env.JWT_SECRET, (error, verifiedToken) => {
                 if (error) {
-                    console.log('jwtMiddlwareError');
+                    console.log(`jwtMiddlwareError: ${error}`);
                     reject(error);
                 }
                 //검증 성공하면 verifiedToken으로 넘기기
@@ -32,25 +33,26 @@ const jwtMiddleware = (req, res, next) => {
     );
 
     const onError = (error) => {
+        console.log(`jwtError: ${error}`);
         return res.send(errResponse(baseResponse.TOKEN_VERIFICATION_FAILURE));
     }
 
-    p.then( async (verifiedToken) => {
+    p.then(async verifiedToken => {
         // 비번 변경시 추가할 곳
         // DB에서 jwt 토큰 유무 검사(로그인 시 생성하며, 로그아웃 시 제거할 것)
-        const loginCheckbyToken = await userService.checkValidAccess(verifiedToken.userIdx);
+        const loginCheckbyToken = await this.UserService.checkValidAccess(verifiedToken.userIdx);
 
         // 로그아웃/회원탈퇴한 유저에 대해 접근하려는 경우
-        if(loginCheckbyToken == null){
+        if (loginCheckbyToken == null){
             return res.send(errResponse(baseResponse.TOKEN_WRONG_ACCESS));
         }
         // 현재 로그인되어있는 유저의 이전 로그인 jwt로 접근하려는 경우
         else if(loginCheckbyToken != token) {
             return res.send(errResponse(baseResponse.TOKEN_VERIFICATION_FAILURE));
         }
-        else
-            req.verifiedToken = verifiedToken;
-            next();
+        req.verifiedToken = verifiedToken;
+        next();
+
     }).catch(onError);
 };
 
